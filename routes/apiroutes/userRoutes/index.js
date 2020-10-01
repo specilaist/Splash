2002778c5c7b708434fe53d7aab9b804601d8687
specilaist <specilaist@msn.com>
2020-09-30 21:26:34 -0700

    Broke fetchUsers into an orm

--- routes/apiroutes/userRoutes/index.js
+++ routes/apiroutes/userRoutes/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const connection = require('../../../config/connection');
+const { findAllUsers, inserUser } = require('../../../model/userQueries');
+const{ fetchUsers } = require('../../../model/userOrm');
 
 router.route('/')
   .get((async (req, res) => {
+    try {
+      const users = await fetchUsers();
+      res.json(users);
+    } catch (e) {
+      res.status(400).json(e);
+    }
     const query = 'SELECT * FROM users;';
     const [rows] = await connection.query(query);
     console.log('I AM ROWS', rows);
@@ -11,7 +19,7 @@ router.route('/')
   .post(async (req, res) => {
     const userInput = req.body;
     const query = 'INSERT INTO users SET username = ?;';
-    const result = await connection.query(query, userInput);
+    const result = await connection.query(findAllUsers, inserUser);
     res.json(result);
   });
 
